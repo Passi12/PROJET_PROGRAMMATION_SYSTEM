666d9bf469f7c32ad63e78be45b588705a5bcb3e
Tc Passi <114927455+Passi12@users.noreply.github.com>
2023-12-21 13:26:38 +0100

    Add files via upload

---
+++ UnitTestProject1/Sprite.cs
@@ -0,0 +1,11 @@
+﻿namespace UnitTestProject1
+{
+    internal class Sprite
+    {
+        public Sprite()
+        {
+        }
+
+        public object PictureBoxLocation { get; internal set; }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Geppetto;
+using Project;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GoToKitchenVAISSELLE()
+        {
+            // Arrange
+            var model = new Model();
+            var commandes = new Commandes(model);
+            var sprite = new Sprite();
+            var expectedX = 1020; // Coordonnée X attendue après le déplacement
+            var expectedY = 230; // Coordonnée Y attendue après le déplacement
+
+            // Act
+            commandes.GoToKitchenVAISSELLE(sprite);
+
+            // Assert
+            // Vérifiez si le sprite s'est déplacé vers la bonne position
+            Assert.AreEqual(expectedX, sprite.PictureBoxLocation);
+            Assert.AreEqual(expectedY, sprite.PictureBoxLocation);
+
+        }
+    }
+
+}
